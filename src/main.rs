e9451ef9cee364e16b284b29f2914554eb05ee91
Jan Hansen <tiujanhansen@gmail.com>
2026-03-03 23:22:39 +0800

    chore : forgot to add these guys

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use leptos::prelude::*;
+use leptos::mount::mount_to_body;
+
+#[component]
+fn App() -> impl IntoView
+{
+    let (count, set_count) = signal(0);
+    view!
+    {
+        <h1> "Hello, World! - Leptos Running fr" </h1>
+        <button on:click= move |_| set_count.update(|x| *x += 1)
+        > 
+            "Click this button to see the number go up fr: "
+            {count}
+        </button>
+
+        <p>
+            "Beep Boop doubled: "
+            {move || count.get() * 2}
+        </p>
+    }
+}
+fn main() 
+{
+    _ = console_error_panic_hook::set_once();
+    mount_to_body(App);
+}
