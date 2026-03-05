a1cf694c994f98c78354e85fc3a8b92a5d379902
Jan Hansen <tiujanhansen@gmail.com>
2026-03-05 10:39:43 +0800

    chore : wrong html formatting lol, and class tests

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,18 @@ fn App() -> impl IntoView
     {
         <h1> "Hello, World! - Leptos Running fr" </h1>
         <button on:click= move |_| set_count.update(|x| *x += 1)
+        class:btn=move || count.get() % 2 == 0
         > 
             "Click this button to see the number go up fr: "
             {count}
         </button>
 
-        <p>
+        <p class=("p-test", move || (count.get() * 2) % 10 == 0)>
             "Beep Boop doubled: "
             {move || count.get() * 2}
         </p>
     }
+
 }
 fn main() 
 {
