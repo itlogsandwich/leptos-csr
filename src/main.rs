d57c78c3b36b3c1374a387566505d1a044167e7b
Jan Hansen <tiujanhansen@gmail.com>
2026-03-05 11:19:34 +0800

    chore : center a div lol

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,31 @@ fn App() -> impl IntoView
     let (count, set_count) = signal(0);
     view!
     {
-        <h1> "Hello, World! - Leptos Running fr" </h1>
-        <button on:click= move |_| set_count.update(|x| *x += 1)
-        class:btn=move || count.get() % 2 == 0
-        > 
-            "Click this button to see the number go up fr: "
-            {count}
-        </button>
-
-        <p class=("p-test", move || (count.get() * 2) % 10 == 0)>
-            "Beep Boop doubled: "
-            {move || count.get() * 2}
-        </p>
+        <div class="btn-ctn">
+            <h1> "Hello, World! - Leptos Running fr" </h1>
+            <button 
+                on:click= move |_| set_count.update(|x| *x += 1)
+                class:btn=move || count.get() % 2 == 0
+            > 
+                "Click this button to it move fr: "
+                {count}
+            </button>
+            <progress 
+                max="100"
+                value=move || count.get()
+            />
+        </div> 
+        
+        <div class="p-ctn">
+            <label class=("p-test", move || (count.get() * 2) % 10 == 0)>
+                "Beep Boop doubled: "
+                {move || count.get() * 2}
+            </label>
+            <progress 
+                max="200"
+                value=move || count.get() * 2
+            />
+        </div>
     }
 
 }
