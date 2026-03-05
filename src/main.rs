46248dc15bc1b63e320f0f43502448461d547310
Jan Hansen <tiujanhansen@gmail.com>
2026-03-05 20:24:09 +0800

    feat : simple ui, and added title

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,78 @@ use leptos::mount::mount_to_body;
 
 
 #[derive(Debug, Clone, Default)]
-pub struct Money
+pub struct Budget
 {
     id: u64,
+    title: String,
     value: f64, 
 }
 
 #[component]
 fn App() -> impl IntoView
 {
-    let (money, set_money) = signal(0.0);
-    let (tracker, set_tracker) = signal(Vec::<Money>::new());
+    let (title, set_title) = signal(String::new());
+    let (amount, set_amount) = signal(0.0);
+    let (budget, set_budget) = signal(Vec::<Budget>::new());
     let (counter, set_counter) = signal(0);
 
+    let total_budget += budget.get();
     view!
     {
         <main>
-            <header>
-                "Financial Tracker"
+            <header id="header-fr">
+                <h1>"Budget Tracker"</h1>
             </header>
-            <section id="input-body" >
-                <input type ="numeric" on:input=move |event| 
-                {
-                    let val = event_target_value(&event).parse::<f64>();
-                    set_money.set(val.expect("Should be money fr"));
-                }/>
 
-                <button on:click=move |_| 
-                {
-                    set_counter.set(counter.get() + 1);
-                    set_tracker.write().push(Money
-                    {
-                        id: counter.get(),
-                        value: money.get(),
-                    });
-                }>
-                    "Add Money"
-                </button>
-            </section>
-            <section id="money-display">
-                <For
-                    each = move || tracker.get()
-                    key = |tracker| tracker.id
-                    children = move |tracker|
-                    {
-                        view!
+            <div id="main-body">
+                <section id="input-body" >
+                    <div id="input-card">
+                        <label for="title-input"> "Title" </label>
+                        <input id="title-input" type="text" on:input=move |event| 
                         {
-                            <div> 
-                                "₱"{tracker.value}
-                            </div>
+                            set_title.set(event_target_value(&event));
+                        }/>
+                        
+                        <label for="amount-input"> "Amount" </label>
+                        <input id="amount-input" type="text" on:input=move |event| 
+                        {
+                            let val = event_target_value(&event).parse::<f64>();
+                            set_amount.set(val.expect("Should be a valid value fr"));
+                        }/>
+                        <button style="margin: 4px;" on:click=move |_| 
+                        {
+                            set_counter.set(counter.get() + 1);
+                            set_budget.write().push(Budget
+                            {
+                                id: counter.get(),
+                                title: title.get(),
+                                value: amount.get(),
+                            });
+                        }>
+                            "Add Budget"
+                        </button>
+                    </div>
+                </section>
+
+                <section id="money-display">
+                    <h1> "Budget Listed" </h1>
+                    <For
+                        each = move || budget.get()
+                        key = |budget| budget.id
+                        children = move |budget|
+                        {
+                            
+                            view!
+                            {
+                                <div class="block">
+                                    <p> {budget.title} </p>
+                                    <p>"₱"{budget.value} </p>
+                                </div>
+                            }
                         }
-                    }
-                />
-            </section>
+                    />
+                </section>
+            </div>
         </main>
     }
 }
