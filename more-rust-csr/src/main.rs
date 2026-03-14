da680063ffa61d1f3d133f7fe6bcda8f821ad971
Jan Hansen <tiujanhansen@gmail.com>
2026-03-14 20:32:26 +0800

    feat : circle go brr

--- more-rust-csr/src/main.rs
+++ more-rust-csr/src/main.rs
@@ -7,10 +7,16 @@ fn App() -> impl IntoView
     let timer_signal = RwSignal::<Option<IntervalHandle>>::new(None);
     let (timer, set_timer) = timer_signal.split();
 
-    let count_signal = RwSignal::<i32>::new(0);
+    let count_signal = RwSignal::<i32>::new(1500);
     let (count,set_count) = count_signal.split();
    
     let one_second = Duration::new(1, 0);
+
+    let clock_offset = move ||
+    {
+        let offset = (1500.0 - count.get() as f64) / 1500.0 * 251.0;
+        offset 
+    };
 
     let time_display = move || 
     {
@@ -28,31 +34,44 @@ fn App() -> impl IntoView
             <div id="container">
                 <div>
                     <div class="card">
+                        <h1> "Pomodoro Clock It" </h1>
                         <div id="clock">
-                            "THIS IS A CLOCK"
+                            <svg viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="40" stroke="black" stroke-width="4" stroke-dasharray="250" stroke-dashoffset=move|| clock_offset().to_string() fill="none"/>
+                            </svg>
                         </div>
                         <h1> {{time_display}} </h1>
                         <div
                             style="display: flex; gap: 10px;"
                         >
-                            <button class="btn">
-                                "Stop"
+                            <button class="btn" on:click=move |_|
+                            {    
+                                if let Some(handler) = timer.get()
+                                {
+                                    handler.clear();
+                                    set_timer.set(None);
+                                    set_count.set(1500);
+                                }
+                                else
+                                {
+                                    set_count.set(1500);
+                                }
+                            }>
+                                "Reset"
+
                             </button>
                             <button class="btn" on:click=move |_| 
                             {
                                 if let Some(handler) = timer.get()
                                 {
-                                    println!("Timer Stopped!");
                                     handler.clear();
                                     set_timer.set(None);
 
                                 }
                                 else
                                 { 
-                                    set_count.set(0);
-                                    if let Ok(h) = set_interval_with_handle(move ||set_count.update(|x| *x +=1 ), one_second)
+                                    if let Ok(h) = set_interval_with_handle(move ||set_count.update(|x| *x -=1 ), one_second)
                                     {
-                                        println!("Timer Starting...!");
                                         set_timer.set(Some(h));
                                     }
                     
