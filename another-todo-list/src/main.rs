d81bd32866e0009551278489a41c94c46791f9b5
Jan Hansen <tiujanhansen@gmail.com>
2026-03-16 08:03:14 +0800

    feat : reactive list prop

--- another-todo-list/src/main.rs
+++ another-todo-list/src/main.rs
@@ -1,22 +1,38 @@
 use leptos::prelude::*;
 use leptos::mount::mount_to_body;
 
+#[derive(Debug, Clone, Default)]
 pub struct Todo 
 {
     id: i64,
     title: String,
     description: String,
-    isComplete: bool,
+    is_complete: bool,
 }
 
 #[component]
-pub fn ListDisplay(title: ReadSignal<String>) -> impl IntoView
+pub fn ListDisplay(list: ReadSignal<Vec<Todo>>) -> impl IntoView
 {
     view!
     {
-        <div class="list">
-            {{title}}
-        </div>
+        <For
+            each=move || list.get()
+            key= |list| list.id
+            children = move |list|
+            {
+                let status = if list.is_complete { "Finished" } else {"Unfinished"};
+
+                view!
+                {
+
+                <div>
+                    <p>{list.title}</p>
+                    <p>{list.description}</p> 
+                    <p>{status}</p>
+                </div>
+                }
+            }
+        />
     }
 }
 
@@ -27,6 +43,8 @@ pub fn App() -> impl IntoView
     // The differing Signal instantiaion is intentional.
     // It's done for practice
 
+    let (count, set_count) = signal(0);
+
     let title_signal = RwSignal::<String>::new(String::from(""));
     let (title, set_title) = title_signal.split();
 
@@ -34,6 +52,11 @@ pub fn App() -> impl IntoView
     let (description, set_description) = description_signal.split();
 
     let (status, set_status) = signal(false);
+
+    //this basically creates a Read Write signal and instantly splits it
+    //or I can just do signal(Vec::<Todo>::new()); and it'll work lol
+    let (list, set_list) = RwSignal::new(Vec::<Todo>::new()).split();
+
 
     view!
     {
@@ -47,8 +70,23 @@ pub fn App() -> impl IntoView
                     <label for="description-input"> "Add more details to it fr" </label>
                     <input id="description-input" type="text" bind:value=(description, set_description)/>
                 </div>
-
-                <ListDisplay title=title/>
+                <div>
+                    <button type="button"
+                    on:click=move |_| 
+                    {
+                        set_count.set(count.get() + 1);
+                        set_list.write().push(
+                        Todo
+                        {
+                            id: count.get(),
+                            title: title.get(),
+                            description: description.get(),
+                            is_complete: false,
+                        });
+                    }
+                    > "Add Todo" </button>
+                </div>
+                <ListDisplay list=list/>
             </div>
         </main>
     }
