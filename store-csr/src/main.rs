af2781ac25684cf4931cf48004917251f94254e2
Jan Hansen <tiujanhansen@gmail.com>
2026-03-25 21:25:23 +0800

    feat : added updated feature, added category info. Update is not fully reactive, will refactor

--- store-csr/src/main.rs
+++ store-csr/src/main.rs
@@ -1,39 +1,58 @@
-use leptos_router::{path, components::{Router, Route, Routes, ParentRoute, A, Outlet}};
-use leptos::mount::mount_to_body;
-use leptos::prelude::*;
+use leptos_router::{hooks::use_params, hooks::use_navigate, params::Params, path, components::{Router, Route, Routes, ParentRoute, A, Outlet}};
+use leptos::{prelude::*, Params, mount::mount_to_body};
 use product::Product;
 
 mod product;
 
-fn remove_product(product_id: i64, set_products: WriteSignal<Vec<Product>>) -> ()
+fn remove_product(product_id: i64, set_products: WriteSignal<Vec<Product>>)
 {
     set_products.write().retain(|p| p.id != product_id)
 }
 
-#[component]
-fn UpdateProduct() -> impl IntoView
+fn clear_fields(
+    set_name: WriteSignal<String>,
+    set_description: WriteSignal<String>,
+    set_category: WriteSignal<String>,
+    set_price: WriteSignal<f64>,
+)
 {
-    view!
-    {
-        <h1> "Update Product" </h1>
-    }
+    set_name.set(String::from(""));
+    set_description.set(String::from(""));
+    set_category.set(String::from(""));
+    set_price.set(0.0);
+}
+
+#[derive(Params, PartialEq)]
+struct IdParams 
+{
+    id: Option<usize>,
 }
 
 #[component]
-fn AddProduct(
-    id_signal: RwSignal<i64>,
+fn UpdateProduct(
     name_signal: RwSignal<String>,
     description_signal: RwSignal<String>,
+    category_signal: RwSignal<String>,
     price_signal: RwSignal<f64>,
     products_signal: RwSignal<Vec<Product>>,
 ) -> impl IntoView
 {
+    let params = use_params::<IdParams>();
+    
+    let product_id = move ||
+    {
+        params
+            .read()
+            .as_ref()
+            .ok()
+            .and_then(|params| params.id)
+            .unwrap_or_default()
+    };
 
-    let (id, set_id) = id_signal.split();
     let (name,  set_name) = name_signal.split();
     let (description, set_description) = description_signal.split();
     let (price, set_price) = price_signal.split();
-
+    let (category, set_category) = category_signal.split();
     let set_products = products_signal.write_only();
 
     view!
@@ -49,6 +68,89 @@ fn AddProduct(
             <label for="description-input"> "Description" </label>
             <input class="input" name="description-input" type="text" bind:value=(description, set_description)/>
 
+            <label for="category-select"> "Description" </label>
+            <select name="category-select" id="categories" bind:value=(category, set_category)>
+                <option value="Food & Beverage"> "Food & Beverage" </option>
+                <option value="Electronics"> "Electronics" </option>
+                <option value="Apparel"> "Apparel" </option>
+                <option value="Miscellaneous"> "Miscellaneous" </option>
+            </select>
+
+            <label for="price-input"> "Price" </label>
+            <input class="input" name="price-input" type="number" prop:value=price on:input=move |e| 
+            {
+                let val = event_target_value(&e)
+                    .parse::<f64>()
+                    .unwrap_or(0.0);
+                set_price.set(val);
+            }
+            />
+            <button class="btn" on:click=move |_|
+            {
+                set_products.write().iter_mut().find(|p| p.id == product_id() as i64).map(|p| 
+                {
+                    *p = Product
+                    {
+                        name: name.get(),
+                        description: description.get(),
+                        category: category.get(),
+                        price: price.get(),
+                        ..*p
+                    }
+                });
+
+                clear_fields(set_name, set_description, set_category, set_price);
+
+                let navigate = use_navigate();
+                navigate("/products", Default::default());
+            }
+            >
+                "Edit Product"
+            </button>
+        </div>
+    }
+}
+
+#[component]
+fn AddProduct(
+    id_signal: RwSignal<i64>,
+    name_signal: RwSignal<String>,
+    description_signal: RwSignal<String>,
+    category_signal: RwSignal<String>,
+    price_signal: RwSignal<f64>,
+    products_signal: RwSignal<Vec<Product>>,
+) -> impl IntoView
+{
+
+    let (id, set_id) = id_signal.split();
+    let (name,  set_name) = name_signal.split();
+    let (description, set_description) = description_signal.split();
+    let (category, set_category) = category_signal.split();
+    let (price, set_price) = price_signal.split();
+    
+    let set_products = products_signal.write_only();
+
+    view!
+    {
+        <div class="card">
+            <div style="display: flex; flex-direction: row-reverse;" >
+                <A href="/products"> "Close" </A> 
+            </div>
+
+            <label for="name-input">"Name"</label>
+            <input class="input" name="name-input" type="text" bind:value=(name, set_name)/>
+
+            <label for="description-input"> "Description" </label>
+            <input class="input" name="description-input" type="text" bind:value=(description, set_description)/>
+
+            <label for="category-select"> "Category" </label>
+            <select name="category-select" id="categories" bind:value=(category, set_category)>
+                <option value="Food & Beverage"> "Food & Beverage" </option>
+                <option value="Electronics"> "Electronics" </option>
+                <option value="Apparel"> "Apparel" </option>
+                <option value="Miscellaneous"> "Miscellaneous" </option>
+            </select>
+
             <label for="price-input"> "Price" </label>
             <input class="input" name="price-input" type="number" prop:value=price on:input=move |e| 
             {
@@ -62,9 +164,14 @@ fn AddProduct(
             {
                 set_id.set(id.get() + 1);
 
-                let product = Product::new(id.get(), name.get(), description.get(), price.get()); 
+                let product = Product::new(id.get(), name.get(), description.get(), category.get(), price.get()); 
 
                 set_products.write().push(product);
+
+                clear_fields(set_name, set_description, set_category, set_price);
+
+                let navigate = use_navigate();
+                navigate("/products", Default::default());
             }
             >
                 "Add Product"
@@ -90,8 +197,9 @@ fn ListDisplay(products_signal: RwSignal<Vec<Product>>) -> impl IntoView
                     <th>"ID Number"</th>
                     <th>"Name" </th>
                     <th>"Description"</th>
+                    <th>"Category"</th>
                     <th>"Price"</th>
-                    <th> "Actions" </th>
+                    <th>"Actions"</th>
                 </tr>
                 <For
                     each=move || products.get()
@@ -104,14 +212,15 @@ fn ListDisplay(products_signal: RwSignal<Vec<Product>>) -> impl IntoView
                                 <td>{product.id}</td>
                                 <td> {product.name}</td>
                                 <td> {product.description}</td>
+                                <td> {product.category}</td>
                                 <td> "$"{product.price}</td>
                                 <td style="margin: 10px;">
                                     <button>
                                         "View"
                                     </button>
-                                    <button>
+                                    <A href=format!("/products/{}",product.id)>
                                         "Edit"
-                                    </button>
+                                    </A>
                                     <button class="btn" type="button" on:click=move|_|
                                         remove_product(product.id, set_products)
                                     >
@@ -133,10 +242,17 @@ fn ListDisplay(products_signal: RwSignal<Vec<Product>>) -> impl IntoView
 #[component]
 fn Home() -> impl IntoView
 {
-    let labels = vec!["Home", "Products"];
+    let labels = ["Home", "Products"];
     view!
     {
         <div class="home-container">
+            <A href="/">
+                <div class="card">
+                    <label>
+                        {labels[0]}
+                    </label>
+                </div>
+            </A>
             <A href="/products">
                 <div class="card">
                     <label>
@@ -148,12 +264,14 @@ fn Home() -> impl IntoView
     }
 }
 
+
 #[component]
 fn App() -> impl IntoView
 {
     let id_signal = RwSignal::new(0);
     let name_signal = RwSignal::new(String::from(""));
     let description_signal = RwSignal::new(String::from(""));
+    let category_signal = RwSignal::new(String::from(""));
     let price_signal = RwSignal::new(0.0);
     let products_signal = RwSignal::new(Vec::<Product>::new());
     
@@ -177,12 +295,23 @@ fn App() -> impl IntoView
                                 id_signal=id_signal 
                                 name_signal=name_signal
                                 description_signal=description_signal
+                                category_signal=category_signal
                                 price_signal=price_signal
                                 products_signal=products_signal 
                             /> 
                         } 
                         />
-                        <Route path=path!(":id") view=UpdateProduct />
+                        <Route path=path!(":id") view=move || view! 
+                        { 
+                                <UpdateProduct 
+                                    name_signal=name_signal
+                                    description_signal=description_signal
+                                    category_signal=category_signal
+                                    price_signal=price_signal
+                                    products_signal=products_signal 
+                                /> 
+                        }
+                        />
                     </ParentRoute>
                 </Routes>
             </main>
@@ -192,5 +321,6 @@ fn App() -> impl IntoView
 
 fn main() 
 {
+    _ = console_error_panic_hook::set_once();
     mount_to_body(App);
 }
--- store-csr/src/product.rs
+++ store-csr/src/product.rs
@@ -4,18 +4,20 @@ pub struct Product
     pub id: i64,
     pub name: String,
     pub description: String, 
+    pub category: String,
     pub price: f64,
 }
 
 impl Product 
 {
-    pub fn new(id: i64, name: String, description: String, price: f64) -> Self 
+    pub fn new(id: i64, name: String, description: String, category: String, price: f64) -> Self 
     {
         Self
         {
             id,
             name,
             description,
+            category,
             price, 
         }
     }
